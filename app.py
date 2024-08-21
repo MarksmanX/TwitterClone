4f5b005e2ed9ae3a12c2b5b28476d9fa387eea39
MarksmanX <mkeph561@gmail.com>
2024-08-21 08:43:10 -0400

    Changed to sqlite server

--- app.py
+++ app.py
@@ -15,8 +15,9 @@ app = Flask(__name__)
 
 # Get DB_URI from environ variable (useful for production/testing) or,
 # if not set there, use development local db.
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    os.environ.get('DATABASE_URL', 'postgresql:///warbler'))
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 
+                                                       'sqlite:///warbler.db')
+
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
